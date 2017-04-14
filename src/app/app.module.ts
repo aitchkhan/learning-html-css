5553aafcf9afbfc2a8ccfbbdc209c46bad57f67a
Haroon Khan <aitchkhan@hkhan.csquareonline.local>
2017-04-14 20:10:32 +0500

    add page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,25 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appArray: Routes = [
-  { path: 'about', component: AboutComponent}
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: '', component: AppComponent },
+  { path: '**', component: PageNotFoundComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,    
   ],
   providers: [],
   bootstrap: [AppComponent]
